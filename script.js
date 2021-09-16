016734f747f3705d11c86d2af5ca8c72137ae189
djli15736 <djli15736@gmail.com>
2021-09-15 22:49:14 -0400

    initial commit of the app

---
+++ script.js
@@ -0,0 +1,19 @@
+let first_item = document.createElement("li");
+let list = document.getElementById("chathistory");
+
+
+var clickHandler = function(eventObject) {
+  let boxValue = document.getElementById("chatbox").value;
+  let list = document.getElementById("chathistory");
+  let item = document.createElement("li");
+  let username = document.getElementById("userbox").value;
+  item.innerHTML = username + ": "+ boxValue;
+  list.appendChild(item);
+}
+
+var clearHandler = function(eventObject) {
+  list.innerHTML = "";
+}
+
+document.querySelector("#chatbutton").addEventListener("click" , clickHandler);
+document.querySelector("#clearbutton").addEventListener("click", clearHandler);
